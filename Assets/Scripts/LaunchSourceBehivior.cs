4a9dbbb128aa0027af4be445d82a778f297e4a54
Jaibeer  S Dugal <jaibeer72@gmail.com>
2019-02-26 15:19:17 +0530

    bullet spwaning and Shooting with trail done.
    -> Bullet fbx imported
    -> basic shooting script
    -> build version 1 done.
    POA
    -> Hit registration to be done
    -> after hit destroy and create new randome points for the targets to appear.

---
+++ Assets/Scripts/LaunchSourceBehivior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchSourceBehivior : MonoBehaviour
+{
+    public float velocity;
+    public Transform bulletsHolder;
+    public GameObject bulletPrefab;
+    public Transform barrel;
+    public float fireRate; 
+    public float nextfire; 
+
+    public void Fire()
+    {
+        if (Time.time > nextfire)
+        {
+            nextfire = Time.time + fireRate;
+
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, barrel.position, Quaternion.identity, bulletsHolder);
+
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * velocity; 
+        }
+    }
+}
